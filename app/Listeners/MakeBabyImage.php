c50433a210c7c01a14db33b0b6a0dda59de25de3
berriestrawberrie <brooke.henriksson@outlook.com>
2025-10-02 22:16:21 +0200

    Finished breeding mechanics and fixed birthing process so that baby images rendered with correct traits

--- app/Listeners/MakeBabyImage.php
+++ app/Listeners/MakeBabyImage.php
@@ -24,10 +24,13 @@ class MakeBabyImage
      */
     public function handle(NewBabyPony $event): void
     {
-        
+        $token = $event->newPony[0]["token"];
+        $baby = Pony::where('token', $token)->get();
+
+
         $colors = ["eyes", "hair", "hair2", "accent", "accent2", "coat"];
         //ALL CURRENTLY AVAILABLE TRAITS
-        $token = $event->newPony[0]["token"];
+        $token = $token;
         $hairs = ["streak", "hfade", "hrainbow"];
         $faces = ["blaze", "ffade", "fvulpine"];
         $none = ["none"];
@@ -35,7 +38,7 @@ class MakeBabyImage
         $uncolored = ["hrainbow", "fvulpine"];
         $breedID = $event->newPony[0]["breedID"];
         //NEXT ISSUE THE SPECIAL TRAITS NEED TO COME FROM THE DATABASE. 
-        $babytrait = $event->newPony[0]["babytrait"];
+        $babytrait = explode(",", $baby[0]["specialtrait"]);
         $traitID = $event->newPony[0]["traitID"];
         $coat = $event->newPony[0]["coat"];
         $hair = $event->newPony[0]["hair"];
@@ -44,37 +47,66 @@ class MakeBabyImage
         $accent = $event->newPony[0]["accent"];
         $accent2 = $event->newPony[0]["accent2"];
 
-        //BUILD THE TRAIT IMAGE
-        if ($babytrait) {
-            //PULL THE IMAGE FROM DB BY AVATAR ID
-            $specialtrait = SpecialTrait::where('traitname', $babytrait)->get();
-            $img = $specialtrait[0][$traitID];
-            //GENERATE GD IMAGE FROM DB BLOB
-            $gdImg = imagecreatefromstring($img);
-            if ($gdImg !== false) {
-                header('Content-Type: image/png');
-                header('Cache-Control', 'max-age=2592000');
-                imageAlphaBlending($gdImg, true);
-                imageSaveAlpha($gdImg, true);
-                //EXTRACT THE SPECIAL TRAIT COLORING FROM ACCENT IF FACE OR BODY
-                if (in_array($babytrait, $faces) || in_array($babytrait, $bodys)) {
-                    //IF SPECIAL TRAIT IS BOY OR FACE USE ACCENT2
-                    list($huer, $hueg, $hueb) = sscanf($accent2, "%02x%02x%02x");
-                } else {
-                    //ELSE IT'S HAIR TRAIT SO COLOR WITH HAIR2
-                    list($huer, $hueg, $hueb) = sscanf($hair2, "%02x%02x%02x");
-                }
-                //RECOLOR THE PONY HAIR TRAIT IMAGE IF HAIR IS NOT RAINBOW
-                if (in_array($babytrait, $uncolored)) {
+        //SCRUB EMPTY LIST FOR BABIES WITH NO VISIBLE GENES
+        $babytrait = array_filter($babytrait);
+        //DECLARE EMPTY TRAITS ALL
+        $hairTraitImg = null;
+        $faceTraitImg = null;
+        $bodyTraitImg = null;
+        $legTraitImg = null;
 
-                    imagepng($gdImg, $file = public_path('ponygen/' . 'ponylistener' . 'specialtrait.png'));
+        //BUILD THE TRAIT IMAGE
+        if (!empty($babytrait)) {
+            //PULL THE IMAGE FROM DB BY AVATAR ID
+            $specialtrait = SpecialTrait::wherein('traitname', $babytrait)->get();
+            for($i=0; $i< count($babytrait); $i++){
+                $img = $specialtrait[$i][$traitID];
+                //GENERATE GD IMAGE FROM DB BLOB
+                $gdImg = imagecreatefromstring($img);
+                if ($gdImg !== false) {
+                    header('Content-Type: image/png');
+                    header('Cache-Control', 'max-age=2592000');
+                    imageAlphaBlending($gdImg, true);
+                    imageSaveAlpha($gdImg, true);
+                    //EXTRACT THE SPECIAL TRAIT COLORING FROM ACCENT IF FACE OR BODY
+                    if (in_array($babytrait[$i], $faces) || in_array($babytrait[$i], $bodys)) {
+                        //IF SPECIAL TRAIT IS BOY OR FACE USE ACCENT2
+                        list($huer, $hueg, $hueb) = sscanf($accent2, "%02x%02x%02x");
+                    } else {
+                        //ELSE IT'S HAIR TRAIT SO COLOR WITH HAIR2
+                        list($huer, $hueg, $hueb) = sscanf($hair2, "%02x%02x%02x");
+                    }
+                    //RECOLOR THE PONY HAIR TRAIT IMAGE IF HAIR IS NOT RAINBOW
+                    if (in_array($babytrait[$i], $uncolored)) {
+
+                        imagepng($gdImg, $file = public_path('ponygen/' . 'ponylistener'.$i . 'specialtrait.png'));
+                    } else {
+                        imagefilter($gdImg, IMG_FILTER_COLORIZE, $huer, $hueg, $hueb);
+                        imagepng($gdImg, $file = public_path('ponygen/' . 'ponylistener' .$i. 'specialtrait.png'));
+                    }
                 } else {
-                    imagefilter($gdImg, IMG_FILTER_COLORIZE, $huer, $hueg, $hueb);
-                    imagepng($gdImg, $file = public_path('ponygen/' . 'ponylistener' . 'specialtrait.png'));
+                    echo 'An error occured retrieving trait image from DB';
                 }
-            } else {
-                echo 'An error occured retrieving trait image from DB';
-            }
+
+                //ATTACH THE IMAGE TO THE CORRECT IMAGE
+                switch($babytrait[$i]){
+                    case "streak":
+                    case "hrainbow":
+                    case "hfade":
+                    $hairTraitImg = $gdImg;
+                    break;
+                    case "blaze":
+                    case "ffade":
+                    case "fvulpine":
+                    $faceTraitImg = $gdImg;
+                    break;
+                    case "paint":
+                    $bodyTraitImg = $gdImg;
+                    break;
+                }
+                
+            }//END OF FOR LOOP
+
         } else {
             $babytrait = null;
         } //END OF TRAIT IMAGE 
@@ -159,29 +191,31 @@ class MakeBabyImage
         imagefilter($accent2Img, IMG_FILTER_COLORIZE, $huer, $hueg, $hueb);
 
 
-        if ($babytrait) {
-            //TRAIT IMAGE IS FACE OR BODY
-            if (in_array($babytrait, $faces) || in_array($babytrait, $bodys)) {
-                imagecopy($baseImg, $gdImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $hairImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $accentImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $accent2Img, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $maskImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $shadeImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $whiteImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $inkImg, 0, 0, 0, 0, 599, 485);
+        if (!empty($babytrait)) {
+            imagecopy($baseImg, $hairImg, 0, 0, 0, 0, 599, 485);
+            imagecopy($baseImg, $accentImg, 0, 0, 0, 0, 599, 485);
+            imagecopy($baseImg, $accent2Img, 0, 0, 0, 0, 599, 485);
+
+            if(!is_null($hairTraitImg)){
+                imagecopy($baseImg, $hairTraitImg, 0, 0, 0, 0, 599, 485);
+                imagedestroy($hairTraitImg);
             }
-            //TRAIT IMAGE IS HAIR
-            if (in_array($babytrait, $hairs)) {
-                imagecopy($baseImg, $hairImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $gdImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $accentImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $accent2Img, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $maskImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $shadeImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $whiteImg, 0, 0, 0, 0, 599, 485);
-                imagecopy($baseImg, $inkImg, 0, 0, 0, 0, 599, 485);
+            if(!is_null($bodyTraitImg)){
+                imagecopy($baseImg, $bodyTraitImg, 0, 0, 0, 0, 599, 485);
+                imagedestroy($bodyTraitImg);
             }
+            if(!is_null($faceTraitImg)){
+                imagecopy($baseImg, $faceTraitImg, 0, 0, 0, 0, 599, 485);
+                imagedestroy($bodyTraitImg);
+            }
+            if(!is_null($legTraitImg)){
+                imagecopy($baseImg, $legTraitImg, 0, 0, 0, 0, 599, 485);
+            }
+            imagecopy($baseImg, $maskImg, 0, 0, 0, 0, 599, 485);
+            imagecopy($baseImg, $shadeImg, 0, 0, 0, 0, 599, 485);
+            imagecopy($baseImg, $whiteImg, 0, 0, 0, 0, 599, 485);
+            imagecopy($baseImg, $inkImg, 0, 0, 0, 0, 599, 485);
+
         } else {
             //NO VISIBLE TRAIT SO EXCLUDE TRAIT IMAGE
             imagecopy($baseImg, $hairImg, 0, 0, 0, 0, 599, 485);
@@ -216,5 +250,6 @@ class MakeBabyImage
         imagedestroy($whiteImg);
         imagedestroy($inkImg);
         imagedestroy($maskImg);
+
     }
 }
